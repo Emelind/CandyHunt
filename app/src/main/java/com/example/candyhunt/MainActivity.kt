d92feb9b21bb535bd7e1715f5c21fa5bb6eba403
Emelind <emeliejlindvall@gmail.com>
2020-09-26 19:02:21 +0200

    Declared 6 TextViews, 1 EditText, 2 Button in main activity (lateinit)

--- app/src/main/java/com/example/candyhunt/MainActivity.kt
+++ app/src/main/java/com/example/candyhunt/MainActivity.kt
@@ -2,8 +2,28 @@ package com.example.candyhunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    // DeclaringTextViews
+    lateinit var titleTextView: TextView
+    lateinit var mainTextView: TextView
+    lateinit var numberTextView: TextView
+    lateinit var alternative1TextView: TextView
+    lateinit var alternative2TextView: TextView
+    lateinit var alternative3TextView: TextView
+
+    // Declaring EditText
+    lateinit var playerNameEditText: EditText
+
+    // Declaring Buttons
+    lateinit var continueButton: Button
+    lateinit var rollDieButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
