4e08fae679133635c976a54e541c7562a14c4a60
Emelind <emeliejlindvall@gmail.com>
2020-09-26 19:08:36 +0200

    Created rollDie function, connected it to RollDieButton and declared number variable to store rolled number in.

--- app/src/main/java/com/example/candyhunt/MainActivity.kt
+++ app/src/main/java/com/example/candyhunt/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.candyhunt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -24,6 +25,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var continueButton: Button
     lateinit var rollDieButton: Button
 
+    // Declaring other variables
+    var number = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -44,4 +48,12 @@ class MainActivity : AppCompatActivity() {
         continueButton = findViewById(R.id.continueButton)
 
     }
+    // Rolls the die and puts the number in the number variable. Prints the number in
+    // the numberTextView. Hides the rollDieButton.
+    fun rollDie(view: View) {
+        number = (1..6).random()
+        numberTextView.text = number.toString()
+        // rollDieButton.visibility = View.INVISIBLE
+    }
+
 }
