c34286dbf7a51fd0703e1e2534877c2eebab2078
Emelind <emeliejlindvall@gmail.com>
2020-09-26 19:30:16 +0200

    Added alternative variables that stores the alternatives from locationNow, added if expression in continueButton that checks the size of alternative list and thereafter decides which alternative should be stored in locationNow depending on number from RollDie fun.

--- app/src/main/java/com/example/candyhunt/MainActivity.kt
+++ app/src/main/java/com/example/candyhunt/MainActivity.kt
@@ -29,6 +29,9 @@ class MainActivity : AppCompatActivity() {
     var number = 0
     var playerName = ""
     lateinit var locationNow: Location
+    var alternative1: Int = -1
+    var alternative2: Int = -1
+    var alternative3: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,7 +55,6 @@ class MainActivity : AppCompatActivity() {
         locationNow = GameManager.locations[0]
         startNewLocation()
 
-
     }
 
     // Rolls the die and puts the number in the number variable. Prints the number in
@@ -73,6 +75,25 @@ class MainActivity : AppCompatActivity() {
         alternative2TextView.text = ""
         alternative3TextView.text = ""
 
+        if(locationNow.alternatives.size == 1) {
+            locationNow = GameManager.locations[alternative1]
+        } else if(locationNow.alternatives.size == 2) {
+            if (number == 1 || number == 2 || number == 3) {
+                locationNow = GameManager.locations[alternative1]
+            } else if (number == 4 || number == 5 || number == 6) {
+                locationNow = GameManager.locations[alternative2]
+            }
+        } else if(locationNow.alternatives.size == 3) {
+            if (number == 1 || number == 2) {
+                locationNow = GameManager.locations[alternative1]
+            } else if (number == 3 || number == 4) {
+                locationNow = GameManager.locations[alternative2]
+            } else if(number == 5 || number == 6) {
+                locationNow = GameManager.locations[alternative3]
+            }
+        }
+        startNewLocation()
+
     }
     fun startNewLocation() {
         // Changes the visibility of the rollDieButton and clears the numberTextView
@@ -83,8 +104,27 @@ class MainActivity : AppCompatActivity() {
         mainTextView.text = locationNow.text
         titleTextView.text = locationNow.title
 
+        // Finds the alternative locations for the new location
+        if(locationNow.alternatives.size == 1) {
+            rollDieButton.visibility = View.INVISIBLE
+            alternative1 = locationNow.alternatives[0]
+            alternative1TextView.text = getString(R.string.next_up, GameManager.locations[alternative1].title)
 
+        } else if(locationNow.alternatives.size == 2) {
+            alternative1 = locationNow.alternatives[0]
+            alternative1TextView.text = getString(R.string.roll_1_to_3, GameManager.locations[alternative1].title)
+            alternative2 = locationNow.alternatives[1]
+            alternative2TextView.text = getString(R.string.roll_4_to_6, GameManager.locations[alternative2].title)
+
+        } else if(locationNow.alternatives.size == 3) {
+            alternative1 = locationNow.alternatives[0]
+            // LÄGG IN SOM OVAN
+            alternative2 = locationNow.alternatives[1]
+            // LÄGG IN SOM OVAN
+            alternative3 = locationNow.alternatives[2]
+            // LÄGG IN SOM OVAN
+        }
     }
-
-
+    // EVENTUELLT - lägg till en number = 0 som kan styra om det går att trycka på continue-button eller inte
+    // eventuellt lägg till en toaster som ger felmeddelande.
 }
