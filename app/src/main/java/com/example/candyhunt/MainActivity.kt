9b08ed579379519f72b2938ccd92bb5a3f4900c5
Emelind <emeliejlindvall@gmail.com>
2020-10-22 08:24:40 +0200

    Added content descriptions in layout

--- app/src/main/java/com/example/candyhunt/MainActivity.kt
+++ app/src/main/java/com/example/candyhunt/MainActivity.kt
@@ -12,7 +12,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 
 const val EXTRA_MESSAGE = "com.example.candyhunt.MESSAGE"
 
